913b78f7362a6fce123ddd3617190eac760638a6
Trevor Killeen <killeentm@gmail.com>
2017-11-06 14:12:02 -0700

    basic pascal annotation xml parsing in dataset

--- datasets/pascal_voc.py
+++ datasets/pascal_voc.py
@@ -1,6 +1,7 @@
+import numpy as np
 import os
 import torch.utils.data as data
-
+import xml.etree.ElementTree as ET
 
 class PascalVOC(data.Dataset):
     """`Pascal VOC <http://host.robots.ox.ac.uk/pascal/VOC/>`_ Dataset.
@@ -44,6 +45,44 @@ class PascalVOC(data.Dataset):
         # example: /VOC2012/Annotations/2008_000128.xml
         return os.path.join(image_dir, img_id + '.xml')
 
+    def _parse_annotation_xml(self, annotation_path):
+        # TODO: ultimately we will want this image metadata to be in a common
+        # format, but for now I will just parse it so I can understand its
+        # components
+
+        # Pascal VOC Annotations are in the form of XML files. The key
+        # components are subtrees with the <object> tag, these store the ground
+        # truth objects in the image, including the class label and the
+        # bounding box, and other metadata
+        tree = ET.parse(annotation_path)
+        objs = tree.findall('object')
+        count = len(objs)
+
+        # Use a 2D array for bounding box coordinates (x1, y1, x2, y2)
+        boxes = np.ndarray((count, 4), dtype=np.uint16)
+
+        # TODO: ross associates class labels with an index, and only stores the
+        # index instead of a string
+        classes = list()
+        
+        # TODO: ross includes more of the metadata, for now we just get classes
+        # and bounding boxes
+        for idx, obj in enumerate(objs):
+            bbox = obj.find('bndbox')
+            
+            # Pascal VOC dataset has image indices 1-base (#tbt to LuaTorch) so
+            # we convert them to be 0-base
+            x1 = int(bbox.find('xmin').text) - 1
+            y1 = int(bbox.find('ymin').text) - 1
+            x2 = int(bbox.find('xmax').text) - 1
+            y2 = int(bbox.find('ymax').text) - 1
+            boxes[idx, :] = [x1, y1, x2, y2]
+            
+            class_label = obj.find('name').text
+            classes.append(class_label)
+
+        return {'boxes': boxes, 'classes': classes}
+
     def __init__(self, root, image_set):
         self.root = root
 
@@ -78,7 +117,7 @@ class PascalVOC(data.Dataset):
         if not os.path.exists(annotation_path):
             raise RuntimeError('Invalid path to annotation')
 
-        return img_path, annotation_path
+        return img_path, self._parse_annotation_xml(annotation_path)
 
     def __len__(self):
         return len(self.image_ids)
