d1a698c38fb08cbfe567c1ce9ad77dace9a779dc
Trevor Killeen <killeent@users.noreply.github.com>
2017-12-11 11:50:43 -0500

    add some more data util functions (#7)

--- torch_detectron/lib/data_utils.py
+++ torch_detectron/lib/data_utils.py
@@ -1,5 +1,5 @@
 def bbox_xywh_hflip(box, image_width):
-    """Horizonatally flip a bounding box"""
+    """Horizonatally flip a bounding box in XYWH format"""
 
     # In a horizontal flip, the y offsets of the image
     # do not change but the x offset is changed so that
@@ -11,4 +11,23 @@ def bbox_xywh_hflip(box, image_width):
 
     offset = image_width - (box[0] + box[2])
     box[0] = offset
+    return box
+
+def bbox_xyxy_hflip(box, image_width):
+    """Horizonatally flip a bounding box in XYXY format"""
+
+    box[0] = image_width - 1 - box[0]
+    box[2] = image_width - 1 - box[2]
+    temp = box[0]
+    box[0] = box[2]
+    box[2] = temp
+
+    return box
+
+def bbox_xywh_to_xyxy(box):
+    """Convert a box in format XYWH to XYXY"""
+
+    box[0] += box[2] - 1
+    box[1] += box[3] - 1
+    
     return box
