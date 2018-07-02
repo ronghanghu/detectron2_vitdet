7b427c089b7a3ee78729053e168f13569c240ace
Francisco Massa <fvsmassa@gmail.com>
2018-07-02 03:42:45 -0600

    Remove NMS from kernels
    Was already implemented in csrc

--- torch_detectron/core/kernels/__init__.py
+++ torch_detectron/core/kernels/__init__.py
@@ -6,7 +6,7 @@ def _load_C_extensions():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     print(this_dir)
     extra_include_paths = [this_dir]
-    sources = ['compute_flow.cpp', 'compute_flow.cu', 'nms.cu']
+    sources = ['compute_flow.cpp', 'compute_flow.cu']
     sources = [os.path.join(this_dir, s) for s in sources]
     return load_ext('detectron_modules', sources, extra_include_paths=extra_include_paths)
 
--- torch_detectron/core/kernels/compute_flow.cpp
+++ torch_detectron/core/kernels/compute_flow.cpp
@@ -13,22 +13,7 @@ at::Tensor compute_flow(const at::Tensor& boxes,
   AT_ERROR("Not implemented on the CPU");
 }
 
-//////////////////
-
-at::Tensor nms_cuda(const at::Tensor boxes, float nms_overlap_thresh);
-
-at::Tensor nms(const at::Tensor boxes, const at::Tensor scores, float nms_overlap_thresh) {
-  if (boxes.type().is_cuda()) {
-    if (boxes.numel() == 0)
-      return torch::CPU(at::kLong).tensor();
-    auto b = at::cat({boxes, scores.unsqueeze(1)}, 1);
-    return nms_cuda(b, nms_overlap_thresh);
-  }
-  AT_ERROR("Not implemented on the CPU");
-}
- 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("compute_flow", &compute_flow, "compute_flow");
-  m.def("nms", &nms, "nms");
 }
  
