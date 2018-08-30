8cbb79ac3570c303bf8dd192e144a26fa06aa5f4
Francisco Massa <fvsmassa@gmail.com>
2018-08-30 02:17:58 -0700

    Start from scratch

--- torch_detectron/layers/compute_flow.py
+++
@@ -1,4 +0,0 @@
-from torch_detectron import _C
-
-# TODO make it work on the CPU
-compute_flow = _C.compute_flow
--- torch_detectron/layers/nms.py
+++
@@ -1,6 +0,0 @@
-# from ._utils import _C
-from torch_detectron import _C
-
-nms = _C.nms
-# nms.__doc__ = """
-# This function performs Non-maximum suppresion"""
