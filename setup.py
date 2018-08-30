502485c2573eb97dca53b1541af263af711a182b
Francisco Massa <fvsmassa@gmail.com>
2018-08-30 02:31:50 -0700

    Add setup.py
    mostly a copy from previous version, with a line commented

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import glob
+import os
+
+import torch
+from torch.utils.cpp_extension import CUDA_HOME
+from torch.utils.cpp_extension import CppExtension
+from torch.utils.cpp_extension import CUDAExtension
+
+from setuptools import find_packages
+from setuptools import setup
+
+requirements = ["torch", "torchvision"]
+
+
+def get_extensions():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    extensions_dir = os.path.join(this_dir, "torch_detectron", "csrc")
+
+    main_file = glob.glob(os.path.join(extensions_dir, "*.cpp"))
+    source_cpu = glob.glob(os.path.join(extensions_dir, "cpu", "*.cpp"))
+    source_cuda = glob.glob(os.path.join(extensions_dir, "cuda", "*.cu"))
+
+    sources = main_file + source_cpu
+    extension = CppExtension
+
+    define_macros = []
+
+    if torch.cuda.is_available() and CUDA_HOME is not None:
+        extension = CUDAExtension
+        sources += source_cuda
+        define_macros += [("WITH_CUDA", None)]
+
+    sources = [os.path.join(extensions_dir, s) for s in sources]
+
+    include_dirs = [extensions_dir]
+
+    ext_modules = [
+        extension(
+            "torch_detectron._C",
+            sources,
+            include_dirs=include_dirs,
+            define_macros=define_macros,
+        )
+    ]
+
+    return ext_modules
+
+
+setup(
+    name="torch_detectron",
+    version="0.1",
+    author="fmassa",
+    url="https://github.com/fairinternal/detectron.pytorch",
+    description="object detection in pytorch",
+    # packages=find_packages(exclude=("configs", "examples", "test",)),
+    # install_requires=requirements,
+    ext_modules=get_extensions(),
+    cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
+)
