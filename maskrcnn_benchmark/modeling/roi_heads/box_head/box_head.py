9c6c657efde41ebcb21e18ee85b7d56bc8b5484e
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-11-10 22:37:43 +0800

    Move subsample logic out of LossEvaluator

--- maskrcnn_benchmark/modeling/roi_heads/box_head/box_head.py
+++ maskrcnn_benchmark/modeling/roi_heads/box_head/box_head.py
@@ -1,6 +1,13 @@
 import torch
 from torch import nn
 
+from maskrcnn_benchmark.structures.boxlist_ops import boxlist_iou
+from maskrcnn_benchmark.modeling.matcher import Matcher
+from maskrcnn_benchmark.modeling.box_coder import BoxCoder
+from maskrcnn_benchmark.modeling.balanced_positive_negative_sampler import (
+    BalancedPositiveNegativeSampler
+)
+
 from .roi_box_feature_extractors import make_roi_box_feature_extractor
 from .roi_box_predictors import make_roi_box_predictor
 from .inference import make_roi_box_post_processor
@@ -19,10 +26,23 @@ class ROIBoxHead(torch.nn.Module):
         self.post_processor = make_roi_box_post_processor(cfg)
         self.loss_evaluator = make_roi_box_loss_evaluator(cfg)
 
+        # match proposals to gt boxes
+        self.proposal_matcher = Matcher(
+            cfg.MODEL.ROI_HEADS.FG_IOU_THRESHOLD,
+            cfg.MODEL.ROI_HEADS.BG_IOU_THRESHOLD,
+            allow_low_quality_matches=False,
+        )
+        bbox_reg_weights = cfg.MODEL.ROI_HEADS.BBOX_REG_WEIGHTS
+        self.box_coder = BoxCoder(weights=bbox_reg_weights)
+
+        self.fg_bg_sampler = BalancedPositiveNegativeSampler(
+            cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE, cfg.MODEL.ROI_HEADS.POSITIVE_FRACTION
+        )
+
     def forward(self, features, proposals, targets=None):
         if self.training:
             with torch.no_grad():
-                proposals = self.loss_evaluator.subsample(proposals, targets)
+                proposals = self._prepare_regression_targets(proposals, targets)
 
         x = self.feature_extractor(features, proposals)
         class_logits, box_regression = self.predictor(x)
@@ -32,13 +52,64 @@ class ROIBoxHead(torch.nn.Module):
             return x, result, {}
 
         loss_classifier, loss_box_reg = self.loss_evaluator(
-            [class_logits], [box_regression]
+            proposals, [class_logits], [box_regression]
         )
         return (
             x,
             proposals,
             dict(loss_classifier=loss_classifier, loss_box_reg=loss_box_reg),
         )
+
+    def _prepare_regression_targets(self, proposals, targets):
+        """
+        Prepare BB-regression targets for all the proposals.
+
+        Args:
+            proposals (list[BoxList]): #img BoxList. Each contains the proposals for the image.
+            targets (list[BoxList]): #img BoxList. The GT boxes for the image.
+
+        Returns:
+            The proposals after sampling, with two additional fields:
+            labels (list[Tensor]): Each is a 1D tensor containing the label (-1, 0, 1) for each proposal.
+            regression_targets: (list[Tensor]):  Each has shape Nx4, the regression targets for the proposals.
+                Only those proposals with positive labels contains meaningful target values.
+        """
+        labels = []
+        for proposals_per_image, targets_per_image in zip(proposals, targets):
+            match_quality_matrix = boxlist_iou(targets_per_image, proposals_per_image)
+            matched_idxs = self.proposal_matcher(match_quality_matrix)
+            # Fast RCNN only need "labels" field for selecting the targets
+            target = targets_per_image.copy_with_fields("labels")
+            # get the targets corresponding GT for each proposal
+            # NB: need to clamp the indices because we can have a single
+            # GT in the image, and matched_idxs can be -2, which goes
+            # out of bounds
+            matched_targets = target[matched_idxs.clamp(min=0)]
+
+            labels_per_image = matched_targets.get_field("labels").to(dtype=torch.int64)
+
+            # Label background (below the low threshold)
+            labels_per_image[matched_idxs == Matcher.BELOW_LOW_THRESHOLD] = 0
+
+            # Label ignore proposals (between low and high thresholds)
+            labels_per_image[matched_idxs == Matcher.BETWEEN_THRESHOLDS] = -1  # -1 is ignored by sampler
+            labels.append(labels_per_image)
+
+            # compute regression targets
+            regression_targets_per_image = self.box_coder.encode(
+                matched_targets.bbox, proposals_per_image.bbox
+            )
+
+            proposals_per_image.add_field("labels", labels_per_image)
+            proposals_per_image.add_field("regression_targets", regression_targets_per_image)
+
+        sampled_pos_inds, sampled_neg_inds = self.fg_bg_sampler(labels)
+        proposals = [
+            p[torch.nonzero(pos_inds | neg_inds).squeeze(1)]
+            for p, pos_inds, neg_inds in zip(proposals, sampled_pos_inds, sampled_neg_inds)
+        ]
+
+        return proposals
 
 
 def build_roi_box_head(cfg):
--- maskrcnn_benchmark/modeling/roi_heads/box_head/loss.py
+++ maskrcnn_benchmark/modeling/roi_heads/box_head/loss.py
@@ -2,9 +2,6 @@ import torch
 from torch.nn import functional as F
 
 from maskrcnn_benchmark.layers import smooth_l1_loss
-from maskrcnn_benchmark.modeling.box_coder import BoxCoder
-from maskrcnn_benchmark.modeling.matcher import Matcher
-from maskrcnn_benchmark.structures.boxlist_ops import boxlist_iou
 from maskrcnn_benchmark.modeling.balanced_positive_negative_sampler import (
     BalancedPositiveNegativeSampler
 )
@@ -17,95 +14,10 @@ class FastRCNNLossComputation(object):
     Also supports FPN
     """
 
-    def __init__(self, proposal_matcher, fg_bg_sampler, box_coder):
-        """
-        Arguments:
-            proposal_matcher (Matcher)
-            fg_bg_sampler (BalancedPositiveNegativeSampler)
-            box_coder (BoxCoder)
-        """
-        self.proposal_matcher = proposal_matcher
-        self.fg_bg_sampler = fg_bg_sampler
-        self.box_coder = box_coder
+    def __init__(self):
+        pass
 
-    def match_targets_to_proposals(self, proposal, target):
-        match_quality_matrix = boxlist_iou(target, proposal)
-        matched_idxs = self.proposal_matcher(match_quality_matrix)
-        # Fast RCNN only need "labels" field for selecting the targets
-        target = target.copy_with_fields("labels")
-        # get the targets corresponding GT for each proposal
-        # NB: need to clamp the indices because we can have a single
-        # GT in the image, and matched_idxs can be -2, which goes
-        # out of bounds
-        matched_targets = target[matched_idxs.clamp(min=0)]
-        return matched_targets, matched_idxs
-
-    def prepare_targets(self, proposals, targets):
-        labels = []
-        regression_targets = []
-        for proposals_per_image, targets_per_image in zip(proposals, targets):
-            matched_targets, matched_idxs = self.match_targets_to_proposals(
-                proposals_per_image, targets_per_image
-            )
-
-            labels_per_image = matched_targets.get_field("labels")
-            labels_per_image = labels_per_image.to(dtype=torch.int64)
-
-            # Label background (below the low threshold)
-            bg_inds = matched_idxs == Matcher.BELOW_LOW_THRESHOLD
-            labels_per_image[bg_inds] = 0
-
-            # Label ignore proposals (between low and high thresholds)
-            ignore_inds = matched_idxs == Matcher.BETWEEN_THRESHOLDS
-            labels_per_image[ignore_inds] = -1  # -1 is ignored by sampler
-
-            # compute regression targets
-            regression_targets_per_image = self.box_coder.encode(
-                matched_targets.bbox, proposals_per_image.bbox
-            )
-
-            labels.append(labels_per_image)
-            regression_targets.append(regression_targets_per_image)
-
-        return labels, regression_targets
-
-    def subsample(self, proposals, targets):
-        """
-        This method performs the positive/negative sampling, and return
-        the sampled proposals.
-        Note: this function keeps a state.
-
-        Arguments:
-            proposals (list[BoxList])
-            targets (list[BoxList])
-        """
-
-        labels, regression_targets = self.prepare_targets(proposals, targets)
-        sampled_pos_inds, sampled_neg_inds = self.fg_bg_sampler(labels)
-
-        proposals = list(proposals)
-        # add corresponding label and regression_targets information to the bounding boxes
-        for labels_per_image, regression_targets_per_image, proposals_per_image in zip(
-            labels, regression_targets, proposals
-        ):
-            proposals_per_image.add_field("labels", labels_per_image)
-            proposals_per_image.add_field(
-                "regression_targets", regression_targets_per_image
-            )
-
-        # distributed sampled proposals, that were obtained on all feature maps
-        # concatenated via the fg_bg_sampler, into individual feature map levels
-        for img_idx, (pos_inds_img, neg_inds_img) in enumerate(
-            zip(sampled_pos_inds, sampled_neg_inds)
-        ):
-            img_sampled_inds = torch.nonzero(pos_inds_img | neg_inds_img).squeeze(1)
-            proposals_per_image = proposals[img_idx][img_sampled_inds]
-            proposals[img_idx] = proposals_per_image
-
-        self._proposals = proposals
-        return proposals
-
-    def __call__(self, class_logits, box_regression):
+    def __call__(self, proposals, class_logits, box_regression):
         """
         Computes the loss for Faster R-CNN.
         This requires that the subsample method has been called beforehand.
@@ -118,11 +30,6 @@ class FastRCNNLossComputation(object):
         class_logits = cat(class_logits, dim=0)
         box_regression = cat(box_regression, dim=0)
         device = class_logits.device
-
-        if not hasattr(self, "_proposals"):
-            raise RuntimeError("subsample needs to be called before")
-
-        proposals = self._proposals
 
         labels = cat([proposal.get_field("labels") for proposal in proposals], dim=0)
         regression_targets = cat(
@@ -150,19 +57,6 @@ class FastRCNNLossComputation(object):
 
 
 def make_roi_box_loss_evaluator(cfg):
-    matcher = Matcher(
-        cfg.MODEL.ROI_HEADS.FG_IOU_THRESHOLD,
-        cfg.MODEL.ROI_HEADS.BG_IOU_THRESHOLD,
-        allow_low_quality_matches=False,
-    )
-
-    bbox_reg_weights = cfg.MODEL.ROI_HEADS.BBOX_REG_WEIGHTS
-    box_coder = BoxCoder(weights=bbox_reg_weights)
-
-    fg_bg_sampler = BalancedPositiveNegativeSampler(
-        cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE, cfg.MODEL.ROI_HEADS.POSITIVE_FRACTION
-    )
-
-    loss_evaluator = FastRCNNLossComputation(matcher, fg_bg_sampler, box_coder)
+    loss_evaluator = FastRCNNLossComputation()
 
     return loss_evaluator
