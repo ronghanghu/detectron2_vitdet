9d476e0b99931a345ef4d953e37e5268a6bfb2f7
Ross Girshick <rbg@fb.com>
2019-02-10 18:47:33 -0800

    matcher cleanup
    Summary: This diff mainly just improves the documentations. It also improves the correct usage asserts.
    
    Reviewed By: ppwwyyxx
    
    Differential Revision: D14012795
    
    fbshipit-source-id: 10b6bcdcfa6fcd1b6e20c490c429ff3547c20953

--- maskrcnn_benchmark/detection/modeling/matcher.py
+++ maskrcnn_benchmark/detection/modeling/matcher.py
@@ -5,15 +5,16 @@ class Matcher(object):
     """
     This class assigns to each predicted "element" (e.g., a box) a ground-truth
     element. Each predicted element will have exactly zero or one matches; each
-    ground-truth element may be assigned to zero or more predicted elements.
+    ground-truth element may be matched to zero or more predicted elements.
 
-    Matching is based on the MxN match_quality_matrix, that characterizes how well
-    each (ground-truth, predicted)-pair match. For example, if the elements are
-    boxes, the matrix may contain box IoU overlap values.
+    The matching is determined by the MxN match_quality_matrix, that characterizes
+    how well each (ground-truth, prediction)-pair match each other. For example,
+    if the elements are boxes, this matrix may contain box intersection-over-union
+    overlap values.
 
-    The matcher returns a tensor of size N containing the index of the ground-truth
-    element m that matches to prediction n. If there is no match, a negative value
-    is returned.
+    The matcher returns a vector of length N containing the index of the ground-truth
+    element m in [0, M) that matches to prediction n in [0, N). If a prediction was
+    not matched to any ground-truth element, then a negative value is returned.
     """
 
     BELOW_LOW_THRESHOLD = -1
@@ -22,16 +23,23 @@ class Matcher(object):
     def __init__(self, high_threshold, low_threshold, allow_low_quality_matches=False):
         """
         Args:
-            high_threshold (float): quality values greater than or equal to
-                this value are candidate matches.
-            low_threshold (float): a lower quality threshold used to stratify
-                matches into three levels:
-                1) matches >= high_threshold
+            high_threshold (float): to match with any ground-truth element, a prediction
+                must have a maximum match quality, over all ground-truth elements,
+                that is >= this threshold.
+            low_threshold (float): a lower maximum quality threshold that is used to
+                stratify predictions into three levels:
+                1) maximum match quality >= high_threshold
+                    These predictions will be matched to a ground-truth element with
+                    which they have maximum overlap.
                 2) BETWEEN_THRESHOLDS matches in [low_threshold, high_threshold)
+                    These "gray zone" predictions are not matched, but they are given
+                    a special value in case downstream code wishes to treat them specially
+                    (e.g., assign these predictions an ignore label).
                 3) BELOW_LOW_THRESHOLD matches in [0, low_threshold)
+                    These predictions are not matched.
             allow_low_quality_matches (bool): if True, produce additional matches
-                for predictions that have only low-quality match candidates. See
-                set_low_quality_matches_ for more details.
+                for predictions with maximum match quality lower than high_threshold.
+                See set_low_quality_matches_ for more details.
         """
         assert low_threshold <= high_threshold
         self.high_threshold = high_threshold
@@ -42,23 +50,22 @@ class Matcher(object):
         """
         Args:
             match_quality_matrix (Tensor[float]): an MxN tensor, containing the
-            pairwise quality between M ground-truth elements and N predicted elements.
+                pairwise quality between M ground-truth elements and N predicted
+                elements. All elements must be >= 0 (due to the us of `torch.nonzero`
+                for selecting indices in :meth:`set_low_quality_matches_`).
 
         Returns:
-            matches (Tensor[int64]): an N tensor where N[i] is a matched gt in
-            [0, M - 1] or a negative value indicating that prediction i could not
-            be matched.
+            matches (Tensor[int64]): a vector of length N, where matches[i] is a matched
+                ground-truth index in [0, M) or a negative value indicating that
+                prediction i could not be matched. A negative value of
+                Matcher.BELOW_LOW_THREHSOLD indicates that the highest quality ground-truth
+                was lower than the low_threshold. A negative value of Matcher.BETWEEN_THRESHOLDS
+                indicates that the highest quality match was between low_threshold and
+                high_threshold.
         """
-        if match_quality_matrix.numel() == 0:
-            # empty targets or proposals not supported during training
-            if match_quality_matrix.shape[0] == 0:
-                raise ValueError(
-                    "No ground-truth boxes available for one of the images " "during training"
-                )
-            else:
-                raise ValueError(
-                    "No proposal boxes available for one of the images " "during training"
-                )
+        assert match_quality_matrix.dim() == 2
+        assert match_quality_matrix.numel() > 0
+        assert torch.all(match_quality_matrix >= 0)
 
         # match_quality_matrix is M (gt) x N (predicted)
         # Max over gt elements (dim 0) to find best gt candidate for each prediction
@@ -82,14 +89,18 @@ class Matcher(object):
     def set_low_quality_matches_(self, matches, all_matches, match_quality_matrix):
         """
         Produce additional matches for predictions that have only low-quality matches.
-        Specifically, for each ground-truth find the set of predictions that have
+        Specifically, for each ground-truth G find the set of predictions that have
         maximum overlap with it (including ties); for each prediction in that set, if
-        it is unmatched, then match it to the ground-truth with which it has the highest
-        quality value.
+        it is unmatched, then match it to the ground-truth G.
+
+        This function implements the RPN assignment case (i) in Sec. 3.1.2 of the
+        Faster R-CNN paper: https://arxiv.org/pdf/1506.01497v3.pdf.
         """
         # For each gt, find the prediction with which it has highest quality
         highest_quality_foreach_gt, _ = match_quality_matrix.max(dim=1)
-        # Find highest quality match available, even if it is low, including ties
+        # Find the highest quality match available, even if it is low, including ties.
+        # Note that the matches qualities must be positive due to the use of
+        # `torch.nonzero`.
         gt_pred_pairs_of_highest_quality = torch.nonzero(
             match_quality_matrix == highest_quality_foreach_gt[:, None]
         )
