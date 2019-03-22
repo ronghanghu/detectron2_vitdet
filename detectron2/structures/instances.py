3cd6db2feae0441839e04186c80ff246e7f36fc9
Yanghan Wang <yanghan@instagram.com>
2019-03-22 11:22:15 -0700

    fix __str__ w/h bug for Instances
    Summary: width and height were displayed incorrectly
    
    Reviewed By: ppwwyyxx
    
    Differential Revision: D14579493
    
    fbshipit-source-id: 33c542a5ab8306af81e32d53b220d55a341d5623

--- detectron2/structures/instances.py
+++ detectron2/structures/instances.py
@@ -164,7 +164,7 @@ class Instances:
     def __str__(self):
         s = self.__class__.__name__ + "("
         s += "num_instances={}, ".format(len(self))
-        s += "image_width={}, ".format(self._image_size[0])
-        s += "image_height={}, ".format(self._image_size[1])
+        s += "image_height={}, ".format(self._image_size[0])
+        s += "image_width={}, ".format(self._image_size[1])
         s += "fields=[{}])".format(", ".join(self._fields.keys()))
         return s
