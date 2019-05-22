7555a437d2b80ccd7247248f7a7f9bbfde6bb610
Yuxin Wu <ppwwyyxx@users.noreply.github.com>
2019-05-22 11:46:33 -0700

    Allow FrozenBN to load from non-fused weights (#182)
    Summary:
    fix #108
    
    When loading a fused model, moving_mean/var will be 1 and 0 and won't affect results.
    
    Pull Request resolved: https://github.com/fairinternal/detectron2/pull/182
    
    Reviewed By: rbgirshick
    
    Differential Revision: D15425219
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: 49854b80a40609228848e1e86d75fc28a37164e7

--- detectron2/layers/batch_norm.py
+++ detectron2/layers/batch_norm.py
@@ -1,25 +1,42 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 
 class FrozenBatchNorm2d(nn.Module):
     """
     BatchNorm2d where the batch statistics and the affine parameters are fixed.
 
-    It contains non-trainable buffers called "weight" and "bias".
-    The two buffers are computed from the original four parameters of BN:
-    mean, variance, scale (gamma), offset (beta).
+    It contains non-trainable buffers called
+    "weight" and "bias", "running_mean", "running_var",
+    initialized to perform identity transformation.
+
+    The pre-trained backbone models from Caffe2 only contain "weight" and "bias",
+    which are computed from the original four parameters of BN.
     The affine transform `x * weight + bias` will perform the equivalent
-    computation of `(x - mean) / std * scale + offset`, but will be slightly cheaper.
-    The pre-trained backbone models from Caffe2 are already in such a frozen format.
+    computation of `(x - mean) / std * scale + offset`.
+    When loading a backbone model from Caffe2, "running_mean" and "running_var"
+    will be left unchanged as identity transformation.
+
+    Other pre-trained backbone models may contain all 4 parameters.
+
+    The forward is implemented by `F.batch_norm(..., training=False)`.
     """
 
     def __init__(self, n):
         super(FrozenBatchNorm2d, self).__init__()
         self.register_buffer("weight", torch.ones(n))
         self.register_buffer("bias", torch.zeros(n))
+        self.register_buffer("running_mean", torch.zeros(n))
+        self.register_buffer("running_var", torch.ones(n))
 
     def forward(self, x):
-        scale = self.weight.reshape(1, -1, 1, 1)
-        bias = self.bias.reshape(1, -1, 1, 1)
-        return x * scale + bias
+        return F.batch_norm(
+            x,
+            self.running_mean,
+            self.running_var,
+            self.weight,
+            self.bias,
+            training=False,
+            eps=1e-12,
+        )
