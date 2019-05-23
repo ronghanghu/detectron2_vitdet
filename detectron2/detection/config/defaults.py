b1cf6a36eb32612ce12e593c7ba24f3e7ba2d0f5
Ria Verma <riaverma@fb.com>
2019-05-23 10:32:33 -0700

    Set _C.MODEL.ROI_HEADS.SCORE_THRESH to 0 by default (T44037064)
    Summary:
    Set _C.MODEL.ROI_HEADS.SCORE_THRESH to 0 by default
    
    Fix https://github.com/fairinternal/detectron2/issues/166
    
    Reviewed By: rbgirshick
    
    Differential Revision: D15418079
    
    fbshipit-source-id: e5cf2cb69b4e1ecb368712e2f31bb4f65d0b2fbf

--- detectron2/detection/config/defaults.py
+++ detectron2/detection/config/defaults.py
@@ -221,6 +221,8 @@ _C.MODEL.ROI_HEADS.POSITIVE_FRACTION = 0.25
 # Minimum score threshold (assuming scores in a [0, 1] range); a value chosen to
 # balance obtaining high recall with not having too many low precision
 # detections that will slow down inference post processing steps (like NMS)
+# A default threshold of 0.0 increases AP by ~0.2-0.3 but significantly slows down
+# inference.
 _C.MODEL.ROI_HEADS.SCORE_THRESH = 0.05
 # Overlap threshold used for non-maximum suppression (suppress boxes with
 # IoU >= this threshold)
