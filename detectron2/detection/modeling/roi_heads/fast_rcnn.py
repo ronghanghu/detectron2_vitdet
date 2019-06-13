fix false negative calculation
Summary: It looks like the logged false negative rate per minibatch is incorrect since we moved the bg class index from `0` to `num_fg_classes`. This diff fixes the calculation and also renames `num_fg_classes` to `bg_class_ind`, which I think is semantically more meaningful in the context in which it's used.

Reviewed By: ppwwyyxx

Differential Revision: D15804919

fbshipit-source-id: 6c1a916492032f742b4a39764fd33d0a6218ae02

detectron2/detection/modeling/roi_heads/fast_rcnn.py
@@ -68,15 +68,15 @@ def fast_rcnn_losses(
 
     loss_cls = F.cross_entropy(pred_class_logits, gt_classes, reduction="mean")
 
-    num_fg_classes = pred_class_logits.shape[1] - 1
+    bg_class_ind = pred_class_logits.shape[1] - 1
 
     # Box delta loss is only computed between the prediction for the gt class k
-    # (if 0 <= k < num_fg_classes) and the target; there is no loss defined on predictions
+    # (if 0 <= k < bg_class_ind) and the target; there is no loss defined on predictions
     # for non-gt classes and background.
     # Empty fg_inds produces a valid loss of zero as long as the size_average
     # arg to smooth_l1_loss is False (otherwise it uses torch.mean internally
     # and would produce a nan loss).
-    fg_inds = torch.nonzero((gt_classes >= 0) & (gt_classes < num_fg_classes)).squeeze(1)
+    fg_inds = torch.nonzero((gt_classes >= 0) & (gt_classes < bg_class_ind)).squeeze(1)
     if cls_agnostic_bbox_reg:
         # pred_proposal_deltas only corresponds to foreground class for agnostic
         gt_class_cols = torch.tensor([0, 1, 2, 3], device=device)
@@ -245,14 +245,14 @@ class FastRCNNOutputs(object):
         """
         num_instances = self.gt_classes.numel()
         pred_classes = self.pred_class_logits.argmax(dim=1)
-        num_fg_classes = self.pred_class_logits.shape[1] - 1
+        bg_class_ind = self.pred_class_logits.shape[1] - 1
 
-        fg_inds = (self.gt_classes >= 0) & (self.gt_classes < num_fg_classes)
+        fg_inds = (self.gt_classes >= 0) & (self.gt_classes < bg_class_ind)
         num_fg = fg_inds.nonzero().numel()
         fg_gt_classes = self.gt_classes[fg_inds]
         fg_pred_classes = pred_classes[fg_inds]
 
-        num_false_negative = (fg_pred_classes == 0).nonzero().numel()
+        num_false_negative = (fg_pred_classes == bg_class_ind).nonzero().numel()
         num_accurate = (pred_classes == self.gt_classes).nonzero().numel()
         fg_num_accurate = (fg_pred_classes == fg_gt_classes).nonzero().numel()
 