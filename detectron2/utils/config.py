d6278c3e784e42b005e630a8911b96110d320b95
Yuxin Wu <yuxinwu@fb.com>
2019-06-26 20:49:42 -0700

    lint/cleanup/fix launcher
    Summary: small changes
    
    Reviewed By: rbgirshick
    
    Differential Revision: D16003623
    
    fbshipit-source-id: 265889a472abea2f4b67d0dc38a0f1bb6f10e771

--- detectron2/utils/config.py
+++ detectron2/utils/config.py
@@ -1,3 +1,3 @@
 # Keep this module for backward compatibility and
 # detectron2-specific config features could be added in this file.
-from borc.common.config import CfgNode
+from borc.common.config import CfgNode  # noqa
