7c04c28c7da64dc3a41de6e3d29e2e7eb9f5a3b8
Yuxin Wu <yuxinwu@fb.com>
2019-08-23 09:19:05 -0700

    Tensor.data -> Tensor.data_ptr
    Summary:
    PyTorch is complaining
    ```
    include/ATen/core/Tensor.h:292: UserWarning: Tensor.data<T>() is deprecated. Please use
    Tensor.data_ptr<T>() instead.
      TORCH_WARN("Tensor.data<T>() is deprecated. Please use Tensor.data_ptr<T>() instead.")
    ```
    and makes development impossible.
    
    This diff also changes NMS in torchvision.
    However, torchvision is not regularly synced between fbcode and github -
    it seems to be manually synced once a while.
    
    We may need to discuss with fmassa (when he's back)
    about the development process of torchvision in case of such issues.
    
    Reviewed By: rbgirshick
    
    Differential Revision: D16982739
    
    fbshipit-source-id: 1c5dcb32873b457db4439b469d729dec31a83e07

--- detectron2/layers/csrc/ROIAlign/ROIAlign_cpu.cpp
+++ detectron2/layers/csrc/ROIAlign/ROIAlign_cpu.cpp
@@ -419,7 +419,7 @@ at::Tensor ROIAlign_forward_cpu(
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(input.type(), "ROIAlign_forward", [&] {
     ROIAlignForward<scalar_t>(
         output_size,
-        input.contiguous().data<scalar_t>(),
+        input.contiguous().data_ptr<scalar_t>(),
         spatial_scale,
         channels,
         height,
@@ -427,8 +427,8 @@ at::Tensor ROIAlign_forward_cpu(
         pooled_height,
         pooled_width,
         sampling_ratio,
-        rois.contiguous().data<scalar_t>(),
-        output.data<scalar_t>(),
+        rois.contiguous().data_ptr<scalar_t>(),
+        output.data_ptr<scalar_t>(),
         aligned);
   });
   return output;
@@ -471,7 +471,7 @@ at::Tensor ROIAlign_backward_cpu(
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(grad.type(), "ROIAlign_forward", [&] {
     ROIAlignBackward<scalar_t>(
         grad.numel(),
-        grad.contiguous().data<scalar_t>(),
+        grad.contiguous().data_ptr<scalar_t>(),
         spatial_scale,
         channels,
         height,
@@ -479,8 +479,8 @@ at::Tensor ROIAlign_backward_cpu(
         pooled_height,
         pooled_width,
         sampling_ratio,
-        grad_input.data<scalar_t>(),
-        rois.contiguous().data<scalar_t>(),
+        grad_input.data_ptr<scalar_t>(),
+        rois.contiguous().data_ptr<scalar_t>(),
         n_stride,
         c_stride,
         h_stride,
