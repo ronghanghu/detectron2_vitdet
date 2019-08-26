5dcb89543bbaa8bb5c4bf2fbab34be66dfeae0d0
Yuxin Wu <yuxinwu@fb.com>
2019-08-26 14:11:32 -0700

    improve linter script
    Summary:
    flake8 may use python2 in users' environment, `python3 -m flake8` is more reliable.
    However it is not available on devgpu ..., so we check for `flake8-3` first.
    
    Reviewed By: rbgirshick
    
    Differential Revision: D17046824
    
    fbshipit-source-id: b3f2181a65551e5274143a219d0a11f53ad1db9e

--- infra/linter.sh
+++ infra/linter.sh
@@ -2,8 +2,17 @@
 
 # Run this script at project root by "./infra/linter.sh" before you commit
 
+echo "Running isort ..."
 isort -y -sp . --skip datasets --atomic
+
+echo "Running black ..."
 black -l 100 .
-flake8 .
+
+echo "Running flake8 ..."
+if [ -x "$(command -v flake8-3)" ]; then
+  flake8-3 .
+else
+  python3 -m flake8 .
+fi
 
 command -v arc > /dev/null && arc lint
