1fb4419e3ab69814b44f1f28c111776dc0acd832
Yuxin Wu <yuxinwu@fb.com>
2019-09-03 13:17:59 -0700

    fix mutability issues about COMPUTED_ config
    Summary:
    Sometimes user do not want the cfg to be modified after building a model.
    But sometimes the opposite. (e.g., in order to access COMPUTED_ attributes afterwards, as in portal's caffe2 conversion)
    
    This diff:
    1. Let users explicitly `cfg.clone()` if do not wish the cfg to be modified by `build_model`.
    2. Modify `borc` so that resetting a COMPUTED_ attr to the same value is no longer an error.
    	This way a user can still easily call `build_model(cfg)` twice.
    
    Reviewed By: rbgirshick, sampepose, wat3rBro
    
    Differential Revision: D17158467
    
    fbshipit-source-id: 30926d783873fefdf3a8b26e48b1e9bb547280b4

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -29,8 +29,6 @@ class GeneralizedRCNN(nn.Module):
         self.backbone = build_backbone(cfg)
         self.proposal_generator = build_proposal_generator(cfg)
         self.roi_heads = build_roi_heads(cfg)
-        # not elegant. Allow downstream to use `model.cfg` to access computed attributes
-        self.cfg = cfg
 
         assert len(cfg.MODEL.PIXEL_MEAN) == len(cfg.MODEL.PIXEL_STD)
         num_channels = len(cfg.MODEL.PIXEL_MEAN)
