9713a18f27297f06e6ff1202c32de5fb80eecb84
Yuxin Wu <yuxinwu@fb.com>
2019-09-11 22:40:58 -0700

    support automatic port selection
    Summary: but not as a default
    
    Reviewed By: rbgirshick
    
    Differential Revision: D17333901
    
    fbshipit-source-id: 0974a95f1f25e54183738b6bcef5c7090bab2cc8

--- detectron2/engine/launch.py
+++ detectron2/engine/launch.py
@@ -8,18 +8,39 @@ from detectron2.utils import comm
 __all__ = ["launch"]
 
 
+def _find_free_port():
+    import socket
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Binding to port 0 will cause the OS to find an available port for us
+    sock.bind(("", 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    # NOTE: there is still a chance the port could be taken by other processes.
+    return port
+
+
 def launch(main_func, num_gpus_per_machine, num_machines=1, machine_rank=0, dist_url=None, args=()):
     """
     Args:
         main_func: a function that will be called by `main_func(*args)`
         num_machines (int): the total number of machines
         machine_rank (int): the rank of this machine (one per machine)
+        dist_url (str): url to connect to for distributed training, including protocal
+                       e.g. "tcp://127.0.0.1:8686".
+                       Can be set to auto to automatically select a free port on localhost
         args (tuple): arguments passed to main_func
     """
     world_size = num_machines * num_gpus_per_machine
     if world_size > 1:
         # https://github.com/pytorch/pytorch/pull/14391
         # TODO prctl in spawned processes
+
+        if dist_url == "auto":
+            assert num_machines == 1, "dist_url=auto cannot work with distributed training."
+            port = _find_free_port()
+            dist_url = f"tcp://127.0.0.1:{port}"
+
         mp.spawn(
             _distributed_worker,
             nprocs=num_gpus_per_machine,
