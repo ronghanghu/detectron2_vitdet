f62ba1ff072c116b4ac415a453ea45692184da96
Yuxin Wu <yuxinwu@fb.com>
2021-04-12 14:41:42 -0700

    use default_setup for new config
    Reviewed By: alexander-kirillov
    
    Differential Revision: D27692660
    
    fbshipit-source-id: a7019f9b97b9b3918bcd129ed02717d77aa0bd68

--- projects_fb/newconfig/main.py
+++ projects_fb/newconfig/main.py
@@ -1,9 +1,5 @@
-import os
-
 from detectron2.config import LazyConfig
-from detectron2.engine import default_argument_parser, launch
-from detectron2.utils.comm import get_rank, synchronize
-from detectron2.utils.logger import setup_logger
+from detectron2.engine import default_argument_parser, default_setup, launch
 
 from defaults import DefaultTrainer
 
@@ -12,16 +8,7 @@ def main(args):
     cfg = LazyConfig.load(args.config_file)
     cfg = LazyConfig.apply_overrides(cfg, args.opts)
 
-    os.makedirs(cfg.train.output_dir, exist_ok=True)  # TODO: call default_setup()
-    setup_logger(cfg.train.output_dir, distributed_rank=get_rank(), name="fvcore")
-    setup_logger(cfg.train.output_dir, distributed_rank=get_rank())
-
-    if get_rank() == 0:
-        # support serialization
-        dump_fname = os.path.join(cfg.train.output_dir, "config.yaml")
-        LazyConfig.save(cfg, dump_fname)
-        # cfg = ConfigFile.load(dump_fname)
-    synchronize()
+    default_setup(cfg, args)
 
     trainer = DefaultTrainer(cfg)
     trainer.resume_or_load(args.resume)
