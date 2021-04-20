52b70cf94efa1e24737ddc1f610c87e6dfa86a2a
Yuxin Wu <yuxinwu@fb.com>
2021-04-19 23:20:48 -0700

    add more py configs
    Reviewed By: alexander-kirillov
    
    Differential Revision: D27809203
    
    fbshipit-source-id: 93daeaffbd0ebb0923191b810f78572338918cf9

--- projects_fb/newconfig/configs/mask_rcnn_R_50_C4_1x.py
+++
@@ -1,9 +0,0 @@
-from detectron2.model_zoo import get_config
-from .common_train import train
-
-optimizer = get_config("common/common_optim.py").SGD
-lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_1x
-dataloader = get_config("common/data/coco.py").dataloader
-model = get_config("common/models/mask_rcnn_c4.py").model
-
-model.backbone.freeze_at = 2
