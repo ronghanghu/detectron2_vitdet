38e6ab8bf5044dbf1614aaa56fc47b9d98f42a55
Hanzi Mao <hannamao@fb.com>
2022-03-03 10:48:19 -0800

    [LVIS]Fix vit_h 4g config

--- projects_fb/MAE/configs/mae_vit/lvis/cascade_mask_rcnn_vit_h_w14_4g_50ep.py
+++ projects_fb/MAE/configs/mae_vit/lvis/cascade_mask_rcnn_vit_h_w14_4g_50ep.py
@@ -8,8 +8,8 @@ from .cascade_mask_rcnn_vit_h_w14_4res_50ep import (
 
 model.backbone.bottom_up.net.residual_block_indexes = []
 model.backbone.bottom_up.net.window_block_indexes = [
-    0, 1, 2, 3, 4,       # 5 global
-    6, 7, 8, 9, 10,      # 11 global
-    12, 13, 14, 15, 16,  # 17 global
-    18, 19, 20, 21, 22,  # 23 global
+    0, 1, 2, 3, 4, 5, 6,          # 7 global
+    8, 9, 10, 11, 12, 13, 14,      # 15 global
+    16, 17, 18, 19, 20, 21, 22,  # 23 global
+    24, 25, 26, 27, 28, 29, 30,   # 31 global
 ]
