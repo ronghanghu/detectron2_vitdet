d710969eb41098174b7838222c734342ccebb4d5
Ronghang Hu <ronghang.hu@gmail.com>
2022-04-04 13:28:38 -0700

    add more configs

---
+++ projects_fb/MAE/configs/mae_vit/rel_high_res/mask_rcnn_vit_b_w14_4g_vitupdown_rel_q_50ep_pretrain448.py
@@ -0,0 +1,32 @@
+from ..mask_rcnn_vit_b_w14_4res_50ep import (
+    dataloader,
+    lr_multiplier,
+    model,
+    train,
+    optimizer
+)
+
+from ....vit.vit import ViTUpDimDown
+
+model.backbone.bottom_up.net.residual_block_indexes = []
+model.backbone.bottom_up.net.window_block_indexes = [
+    0,
+    1,  # 2  global
+    3,
+    4,  # 5  global
+    6,
+    7,  # 8  global
+    9,
+    10,  # 11 global
+]
+
+model.backbone.bottom_up.update(_target_=ViTUpDimDown)
+
+
+# use rel
+model.backbone.bottom_up.net.rel_q = True
+
+# ask the user to specify the checkpoint files
+train.init_checkpoint = "Please specify"
+
+model.backbone.bottom_up.net.pretrain_img_size = 448
