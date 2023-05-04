4608000b77822db716ea9fe31bd7bc09a03cf911
Ronghang Hu <ronghang.hu@gmail.com>
2023-05-04 09:35:11 -0700

    adding ViT-H evaluation configs

---
+++ projects_fb/MAE/configs/mae_vit/rel/mask_rcnn_vit_h_16_w14_4g_allwd_vitupdown_rel_q_50ep.py
@@ -0,0 +1,22 @@
+from ..mask_rcnn_vit_h_16_w14_4res_allwd_50ep import (
+    dataloader,
+    lr_multiplier,
+    model,
+    train,
+    optimizer
+)
+
+from ....vit.vit import ViTUpDimDown
+
+model.backbone.bottom_up.net.residual_block_indexes = []
+model.backbone.bottom_up.net.window_block_indexes = [
+    0, 1, 2, 3, 4, 5, 6,          # 7 global
+    8, 9, 10, 11, 12, 13, 14,     # 15 global
+    16, 17, 18, 19, 20, 21, 22,   # 23 global
+    24, 25, 26, 27, 28, 29, 30,   # 31 global
+]
+
+model.backbone.bottom_up.update(_target_=ViTUpDimDown)
+
+# use rel
+model.backbone.bottom_up.net.rel_q = True
